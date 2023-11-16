13bf621a091e11046863467ce5ba871a75f97579
SongKheang <songkheang11@gmail.com>
2023-11-16 23:38:47 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Internet-Programing/tree/hostTp02',    
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  
 })
